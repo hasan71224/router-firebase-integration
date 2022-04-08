9414d62975623cb5b09d822a8e749b76e03c47b2
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-04-09 00:46:50 +0600

    add useFirebase custom hook and add firebase for user signin and login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase()
     return (
         <div>
-            <h1>login page</h1>
+            <h3>Please Login</h3>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" id='' name='' placeholder='Your Password' />
+                <br />
+                <input type="submit" value="LogIn" />
+            </form>
         </div>
     );
 };
